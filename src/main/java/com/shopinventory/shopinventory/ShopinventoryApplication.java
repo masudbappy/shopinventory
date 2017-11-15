9137cdbd864379106e932d194d074464f5895120
masudbappy <masud.bappy90@gmail.com>
2017-11-15 17:08:30 +0600

    Open project for shop inventory

---
+++ src/main/java/com/shopinventory/shopinventory/ShopinventoryApplication.java
@@ -0,0 +1,12 @@
+package com.shopinventory.shopinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopinventoryApplication.class, args);
+	}
+}
