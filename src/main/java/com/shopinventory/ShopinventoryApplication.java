cd556a14bc39303015bbde8febd76bdbd8bec818
masudbappy <masud.bappy90@gmail.com>
2017-11-18 23:44:33 +0600

    add home page login system

--- src/main/java/com/shopinventory/ShopinventoryApplication.java
+++ src/main/java/com/shopinventory/ShopinventoryApplication.java
@@ -1,4 +1,4 @@
-package com.shopinventory.shopinventory;
+package com.shopinventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/shopinventory/ShopinventoryApplicationTests.java
+++ src/test/java/com/shopinventory/ShopinventoryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.shopinventory.shopinventory;
+package com.shopinventory;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
